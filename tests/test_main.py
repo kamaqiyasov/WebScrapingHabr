23b8a863ddcc8033c4f79a270e47ee41c7852c79
kamaqiyasov <kamil181994@gmail.com>
2025-11-19 13:34:56 +0300

    add setUp params

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import find_in_text, get_url_soup, KEYWORDS, URL
+from main import find_in_text, get_url_soup
 from bs4 import BeautifulSoup
 from fake_headers import Headers
 
@@ -9,27 +9,29 @@ class TestMain(unittest.TestCase):
     def setUp(self):
         headers = Headers(browser="chrome", os="win", headers=True)
         self.header = headers.generate()
-    
+        self.keywords = ['дизайн', 'фото', 'web', 'python']
+        self.url = "https://habr.com"
+        
     def tearDown(self):
         del self.header
     
     def test_find_in_text_true(self):
         text = "Это текст о дизайне и фото"
-        res = find_in_text(KEYWORDS, text)
+        res = find_in_text(self.keywords, text)
         self.assertTrue(res)
         
     def test_find_in_text_false(self):
         text = "Это простой текст о чем то ..."
-        res = find_in_text(KEYWORDS, text)
+        res = find_in_text(self.keywords, text)
         self.assertFalse(res)
         
     def test_find_in_empty_text_false(self):
         text = ""
-        res = find_in_text(KEYWORDS, text)
+        res = find_in_text(self.keywords, text)
         self.assertFalse(res)
         
     def test_url_connection_true(self):
-        soup = get_url_soup(URL, self.header)
+        soup = get_url_soup(self.url, self.header)
         self.assertIsInstance(soup, BeautifulSoup)
         
     def test_url_connection_false(self):
